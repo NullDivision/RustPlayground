7c9f8d8a88c376afd928e4bae897111cdcc0a1f5
Tobias Elod-Zoltan <etobias@pentalog.com>
2020-04-18 16:22:43 +0300

    feat: add basic server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, web};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(
+        || App::new().service(web::resource("/").to(|| HttpResponse::Ok()))
+    )
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
